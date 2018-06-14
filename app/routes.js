c242dbcaafaed362593cf757ac8f6224f84e263f
cristian.ames <cristian.ames@wolox.com.ar>
2018-06-14 14:52:40 -0300

    Validate fields

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,14 @@
 // const controller = require('./controllers/controller');
 
+function hasAllParameters(body) {
+  return (
+    typeof body.email !== 'undefined' &&
+    typeof body.password !== 'undefined' &&
+    typeof body.firstName !== 'undefined' &&
+    typeof body.lastName !== 'undefined'
+  );
+}
+
 function isValidMail(mail) {
   const re = /[A-Z0-9._%+-]*@wolox.com.ar/;
   return re.test(mail);
@@ -7,6 +16,11 @@ function isValidMail(mail) {
 
 exports.init = app => {
   app.post('/users', function(req, res) {
+    // Validate fields
+    if (!hasAllParameters(req.body)) {
+      res.status(400).send('Missing parameters!');
+    }
+
     // Validate email domain
     if (!isValidMail(req.body.email)) {
       res.status(400).send('Invalid email!');
