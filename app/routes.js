8d50019ba444eb7b400db7fcb4831c656e83ab32
cristian.ames <cristian.ames@wolox.com.ar>
2018-06-14 14:04:05 -0300

    Add route with email validation

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,17 @@
 // const controller = require('./controllers/controller');
 
-exports.init = app => {};
+function isValidMail(mail) {
+  const re = /[A-Z0-9._%+-]*@wolox.com.ar/;
+  return re.test(mail);
+}
+
+exports.init = app => {
+  app.post('/users', function(req, res) {
+    // Validate email domain
+    if (!isValidMail(req.body.email)) {
+      res.status(400).send('Invalid email!');
+    }
+
+    res.send('POST Successful!');
+  });
+};
