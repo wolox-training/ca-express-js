50725f73c931c825f85a5fa632278d1b71dc2581
cristian.ames <cristian.ames@wolox.com.ar>
2018-07-10 16:56:34 -0500

    Dictum file plus endpoint tests

--- test/controllers/users.js
+++ test/controllers/users.js
@@ -33,5 +33,71 @@ describe('users controller', () => {
           });
       });
     });
+
+    context('When trying to create a user using invalid data', () => {
+      context('Repeated email', () => {
+        beforeEach('insert previous entry', done => {
+          chai
+            .request(server)
+            .post(NEW_USER_ENDPOINT)
+            .send(params)
+            .then(res => {
+              res.should.have.status(201);
+              done();
+            });
+        });
+
+        it('returns status 500', done => {
+          chai
+            .request(server)
+            .post(NEW_USER_ENDPOINT)
+            .send(params)
+            .catch(res => {
+              res.status.should.be.eq(500);
+              res.response.body.should.have.property('message');
+              done();
+            });
+        });
+      });
+      context('Invalid password', () => {
+        const invalidParams = {
+          email: 'crisasd2@wolox.com.ar',
+          password: 'invalid',
+          first_name: 'Cristian',
+          last_name: 'Ames'
+        };
+
+        it('returns status 500', done => {
+          chai
+            .request(server)
+            .post(NEW_USER_ENDPOINT)
+            .send(invalidParams)
+            .catch(res => {
+              res.status.should.be.eq(500);
+              res.response.body.should.have.property('message');
+              done();
+            });
+        });
+      });
+      context('Missing parameters', () => {
+        const invalidParams = {
+          email: 'crisasd2@wolox.com.ar',
+          password: 'yassord1',
+          first_name: 'Cristian'
+        };
+
+        it('returns status 500', done => {
+          chai
+            .request(server)
+            .post(NEW_USER_ENDPOINT)
+            .send(invalidParams)
+            .catch(res => {
+              res.status.should.be.eq(500);
+              res.response.body.should.have.property('message');
+              done();
+            });
+        });
+      });
+    });
   });
 });
