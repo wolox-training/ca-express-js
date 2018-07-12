1385e04281fd8c4f5f160d38e4dd4827258c1db0
cristian.ames <cristian.ames@wolox.com.ar>
2018-07-11 21:21:06 -0500

    Add tests

--- test/controllers/users.js
+++ test/controllers/users.js
@@ -6,7 +6,9 @@ const fs = require('fs'),
   chaiHttp = require('chai-http'),
   server = require('../../app'),
   dictum = require('dictum.js'),
-  NEW_USER_ENDPOINT = '/users';
+  NEW_USER_ENDPOINT = '/users',
+  NEW_SESSION_ENDPOINT = '/users/sessions',
+  User = require(`../../app/models`).user;
 
 chai.should();
 
@@ -90,6 +92,85 @@ describe('users controller', () => {
           chai
             .request(server)
             .post(NEW_USER_ENDPOINT)
+            .send(invalidParams)
+            .catch(res => {
+              res.status.should.be.eq(500);
+              res.response.body.should.have.property('message');
+              done();
+            });
+        });
+      });
+    });
+  });
+  describe(`${NEW_SESSION_ENDPOINT} POST`, () => {
+    beforeEach('create valid user', done => {
+      User.create({
+        firstName: 'UserName',
+        lastName: 'UserLastName',
+        email: 'validUser@wolox.com.ar',
+        password: 'passord1'.hashCode()
+      }).then(() => done());
+    });
+    context('When requesting with a valid user', () => {
+      const params = {
+        email: 'validUser@wolox.com.ar',
+        password: 'passord1'
+      };
+      it('return status 201', done => {
+        chai
+          .request(server)
+          .post(NEW_SESSION_ENDPOINT)
+          .send(params)
+          .then(res => {
+            res.should.have.status(201);
+            dictum.chai(res, 'Endpoint to create a new user session');
+            done();
+          });
+      });
+    });
+    context('When requesting with invalid data', () => {
+      context('Invalid email', () => {
+        const invalidParams = {
+          email: 'invalid@wolox.com.ar',
+          password: 'passord1'
+        };
+        it('return status 500', done => {
+          chai
+            .request(server)
+            .post(NEW_SESSION_ENDPOINT)
+            .send(invalidParams)
+            .catch(res => {
+              res.status.should.be.eq(500);
+              res.response.body.should.have.property('message');
+              done();
+            });
+        });
+      });
+      context('Invalid password', () => {
+        const invalidParams = {
+          email: 'validUser@wolox.com.ar',
+          password: 'invalid'
+        };
+        it('return status 500', done => {
+          chai
+            .request(server)
+            .post(NEW_SESSION_ENDPOINT)
+            .send(invalidParams)
+            .catch(res => {
+              res.status.should.be.eq(500);
+              res.response.body.should.have.property('message');
+              done();
+            });
+        });
+      });
+      context('Missing parameters', () => {
+        const invalidParams = {
+          email: 'validUser@wolox.com.ar'
+        };
+        it('return status 500', done => {
+          chai
+            .request(server)
+            .post(NEW_SESSION_ENDPOINT)
             .send(invalidParams)
             .catch(res => {
               res.status.should.be.eq(500);
