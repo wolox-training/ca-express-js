9743ff0fa8cdb866fe1b715796a8c8452ee774a5
cristian.ames <cristian.ames@wolox.com.ar>
2018-07-12 19:22:47 -0500

    add pagination

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -41,12 +41,27 @@ exports.authenticate = (req, res, next) => {
     .catch(reason => next(errors.defaultError(`Database error - ${reason}`)));
 };
 exports.list = (req, res, next) => {
-  jwt.verify(req.headers.session_token, JWT_KEY, function(err, decoded) {
+  jwt.verify(req.headers.session_token, JWT_KEY, function(jwtError, decoded) {
     if (decoded != null) {
-      // Paginated response goes here
-      res.status(201).send('Yey!');
-    } else if (err != null) {
-      next(errors.defaultError(`Decoding error - ${err}`));
+      User.findAndCountAll()
+        .then(data => {
+          const page = req.query.page || 0, // page number
+            limit = req.query.limit || 0, // number of records per page
+            pages = Math.ceil(data.count / limit),
+            offset = limit * (page - 1);
+          User.findAll({
+            limit,
+            offset,
+            order: [['id', 'ASC']]
+          }).then(users => {
+            res.status(201).json({ result: users, count: data.count, pages });
+          });
+        })
+        .catch(function(error) {
+          next(errors.defaultError(`Database error - ${error}`));
+        });
+    } else if (jwtError != null) {
+      next(errors.defaultError(`Decoding error - ${jwtError}`));
     } else {
       next(errors.defaultError(`Unknown error while decoding`));
     }
