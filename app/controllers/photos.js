53685bc1923ab481817da15b3d0b94d6bb8d3d6c
cristian.ames <cristian.ames@wolox.com.ar>
2018-08-13 16:34:07 -0300

    remove unnecesary brackets from controllers

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -2,9 +2,8 @@ const errors = require('../errors'),
   albumsService = require('../services/albums');
 require('dotenv').config();
 
-exports.list = (req, res, next) => {
-  return albumsService
+exports.list = (req, res, next) =>
+  albumsService
     .getPhotosforPurchasedAlbum(req.body.token_data.user_id, req.params.id)
     .then(data => res.status(201).json(data))
     .catch(error => next(errors.defaultError(`Database error - ${error}`)));
-};
